902692cdd5f321c21a0a5b54a3f8204c1ad0acd6
babinicz <babinicz@onet.pl>
2022-05-18 18:26:03 +0200

    zmiany

--- src/components/CompanySection/CompanySection.jsx
+++ src/components/CompanySection/CompanySection.jsx
@@ -8,32 +8,68 @@ const CompanySection = () => {
       <Row className="company-section">
         <Col lg="2" md="3">
           <h3 className="d-flex align-items-center gap-1 justify-content-center">
-            <i class="ri-facebook-line"></i>
+            <a
+              href="//www.facebook.com/watch/?ref=search&v=419829542909727&external_log_id=2f37c8e8-2194-4a72-8483-418e1fa502a0&q=czarna%20dziura"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="ri-facebook-line"></i>
+            </a>
           </h3>
         </Col>
         <Col lg="2" md="3">
           <h3 className="d-flex align-items-center gap-1 justify-content-center">
-            <i class="ri-pinterest-line"></i>
+            <a
+              href="//pl.pinterest.com/levy0528/czarne-dziury"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="ri-pinterest-line"></i>
+            </a>
           </h3>
         </Col>
         <Col lg="2" md="3">
           <h3 className="d-flex align-items-center gap-1 justify-content-center">
-            <i class="ri-google-fill"></i>
+            <a
+              href="//brainboss.pl/kosmos/czarna-dziura-czym-jest-i-jak-powstaje"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="ri-google-fill"></i>
+            </a>
           </h3>
         </Col>
         <Col lg="2" md="3">
           <h3 className="d-flex align-items-center gap-1 justify-content-center">
-            <i class="ri-twitter-line"></i>
+            <a
+              href="//twitter.com/cnkopernik/status/1115969180522680320"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="ri-twitter-line"></i>
+            </a>
           </h3>
         </Col>
         <Col lg="2" md="3">
           <h3 className="d-flex align-items-center gap-1 justify-content-center">
-            <i class="ri-youtube-line"></i>
+            <a
+              href="//www.youtube.com/watch?v=SgK0A0ijXvs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="ri-youtube-line"></i>
+            </a>
           </h3>
         </Col>
         <Col lg="2" md="3">
           <h3 className="d-flex align-items-center gap-1 justify-content-center">
-            <i class="ri-instagram-line"></i>
+            <a
+              href="//www.instagram.com/explore/tags/czarnadziura"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="ri-instagram-line"></i>
+            </a>
           </h3>
         </Col>
       </Row>
